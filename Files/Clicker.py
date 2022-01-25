94a11547e8aa0c9fe8c15d6c416229c2c505e099
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2022-01-25 20:17:24 +0000

    0.1.5

--- Files/Clicker.py
+++ Files/Clicker.py
@@ -1,6 +1,8 @@
 import pygame
 import ctypes
 import math
+import ast
+from threading import Thread
 
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
@@ -32,7 +34,8 @@ nana_bek_rect.move_ip(x - 231, y - 280.5)
 # Load Auto Images
 auto_test_img = pygame.image.load("images/Autos/test.png")
 
-
+# def stuff
+balance = 0
 
 
 
@@ -45,6 +48,7 @@ def encode(val, charset):
         tempEnc = encoded
         encoded = tempEnc + str(charset[0] + charset[1].index(str(val[idx])))
     return encoded
+
 
 def decode(val, charset):
     charNum = 0
@@ -59,22 +63,32 @@ def decode(val, charset):
 
 
 # Define Autos / Buildings / do clicking for you
+
 class Autos():
     def __init__(self, datalist):
-        name = datalist[1]
-        self.total_autos = 0
-        self.autos = []
+        total,  autos_all, name = datalist
+        autos_stuff = datalist[1]
+        self.total_autos = int(total)
+        self.autos = autos_all
+        self.autos_xy = []
         self.auto_data = []
+
+        # Set all upgrades to 1
+        self.up_test = 1
+
+
+
         self.auto_rects = []
         self.username = name
+
         print("Auto Class Made")
 
     def New_Auto(self, Name, Cost, NSPps): # image files must be 50 x 50 pixels and JUST say the image name aka Clicker.png
         global sx
         y_coord = self.total_autos * 50
         x_coord = sx - 100
-        self.autos.append((Name, Cost, x_coord, y_coord))
-        self.auto_data.append((Name, NSPps))
+        self.autos_xy.append((Name, Cost, x_coord, y_coord))
+        self.auto_data.append((Name, NSPps, Cost))
         x_pos = x_coord
         y_pos = y_coord
         width = 100
@@ -87,7 +101,7 @@ class Autos():
 
 
     def BLIT(self):
-        for auto in self.autos:
+        for auto in self.autos_xy:
             N, C, x, y = auto
             if N == "test":
                 win.blit(auto_test_img, (x, y))
@@ -107,22 +121,44 @@ class Autos():
                 i += 1
             try:
                 i += Clicked # Test if Anything Is clicked
-                name, amount = self.auto_data[Clicked]
+                name, NSSps, Cost = self.auto_data[Clicked]
+                global balance
                 if name == "test":
-                    pass
-            except:
-                pass
+                    if balance >= Cost:
+                        balance -= Cost
+                        self.autos.append("test")
+                        self.total_autos += 1
+                    
+
+            except Exception as e:
+                print(e)
 
     def Get_Data(self):
-        data_tuple = (self.total_autos, self.autos, self.username)
+        data_list = [self.total_autos, self.autos, self.username]
+        return data_list
+
+    def Tick(self):
+        global balance
+        tickrate = 20
+        for auto in self.autos:
+            if auto == "test":
+                balance += (0.2 * self.up_test) / tickrate
 
 
 
-def save():
+def save(auto):
     # Save Money
     global balance
-    encoded = encode(str(balance), code)
+    cbalance = balance.__round__()
+    encoded = encode(str(cbalance), code)
     f = open("Data/money.txt", "w")
+    f.write(encoded)
+    f.close()
+
+    # Save Data On Autos
+    data = auto.Get_Data()
+    encoded = encode(str(data), code)
+    f = open("Data/datalist.txt", "w")
     f.write(encoded)
     f.close()
 
@@ -140,6 +176,7 @@ def set_text(string, coordx, coordy, fontSize): #Function to set text
 
 def display_info(balance):
     global screen_middle
+    balance = balance.__round__()
     x = screen_middle[0]
     if balance <= 1:
         bal = str(balance) + " Nana's Silver Spoon"
@@ -148,6 +185,10 @@ def display_info(balance):
     s = set_text(bal, x, 100, 20)
     win.blit(s[0], s[1])
 
+
+def clicked_animation_handler(mouse_x, mouse_y):
+    pass
+
 # Getting All Needed Data For Game. - MAIN BRANCH
 
 # Get / Decode Money - SUB BRANCH
@@ -155,13 +196,14 @@ money_file = open("Data/money.txt", "r")
 balance_encoded = money_file.read()
 money_file.close()
 balance = decode(balance_encoded, code)
+balance = float(balance).__round__()
 balance = int(balance)
 
 data_f = open("Data/datalist.txt")
 data = data_f.read()
 data_f.close()
 datafile = decode(data, code)
-
+datafile = ast.literal_eval(datafile)
 
 # Init Autos
 auto = Autos(datafile)
@@ -172,7 +214,7 @@ auto.New_Auto("test", 10, 1)
 # Tests Before Game Loads
 
 
-save()
+save(auto)
 
 
 run = True
@@ -181,6 +223,7 @@ while run:
     win.blit(background,(0, 0))
     win.blit(nana_bek, nana_bek_rect)
     auto.BLIT()
+    auto.Tick()
     display_info(balance)
 
 
@@ -195,13 +238,15 @@ while run:
                 x, y = pygame.mouse.get_pos()
                 if nana_bek_rect.collidepoint(x, y):
                     balance += 1
+                    thread = Thread(target=clicked_animation_handler, args=(x,y))
+                    thread.start()
                 auto.check_click(x, y)
 
     pygame.display.flip()
     clock.tick(20) # finsih tick
     TICKS += 1
     if TICKS >= 100:
-        save()
+        save(auto)
         TICKS = 0
 
 pygame.quit()
