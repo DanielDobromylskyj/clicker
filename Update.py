72c0d95ece395d82f65976eab6ca648f84e7a8d8
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2022-01-28 18:52:41 +0000

    Updater Has Been Released!
    So no longer do you need to manualy move files or delete your progress with this updater! Sadly you will have to reset 1 last time *.
    
    * This may not work and if so u will need to re-download it

---
+++ Update.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+import time
+from distutils.dir_util import copy_tree
+
+
+there = os.path.exists("downloaded/clicker-main/Files/Data/version.txt")
+
+if there == True:
+    v1 = open("downloaded/clicker-main/Files/Data/version.txt", "r")
+    v1_r = v1.read()
+    v1.close()
+
+    v2 = open("Files/Data/version.txt")
+    v2_r = v2.read()
+    v2.close()
+
+    if float(v1_r) > float(v2_r):
+        user_input = input("Valid Update Detected! (" + v1_r + ") Input 0 to Cancel, Input 1 to Update")
+
+        if user_input == "1":
+
+            ui = input("Are you sure? (y/n)")
+
+            if ui == "y":
+                f = open("downloaded/clicker-main/Files/Data/install.txt", "r") # All Current Possible Stuff Is: 0 = main, 1 = image, 2 = data, 3 = Updater
+                install_data = f.read()
+                f.close()
+
+                test = install_data
+
+                for element in range(0, len(test)):
+                    chunk = test[element]
+
+                    if chunk == "0":
+                        shutil.copy("downloaded/clicker-main/Files/Clicker.py", "Files/Clicker.py")
+
+                    if chunk == "1":
+                        copy_tree("downloaded/clicker-main/Files/images", "Files/images")
+
+                    if chunk == "3":
+                        shutil.copy("downloaded/clicker-main/Update.py", "Update.py")
+
+                print("Update Complete! Cleaning Up")
+
+                shutil.rmtree('downloaded/clicker-main')
+
+                print("Clean Up Complete. Exiting")
+                time.sleep(3)
+            elif ui == "n":
+                print("Stopped Update.")
+                time.sleep(3)
+
+            else:
+                print("Failed / Bad Input")
+                time.sleep(3)
+
+
+
+    elif float(v1_r) == float(v2_r):
+        print("Both Versions Are The Same.")
+
+
+    elif float(v1_r) < float(v2_r):
+        print("Out-Dated Version. Exiting")
+        time.sleep(3)
+else:
+    print("No Downloaded Update Detected. Is It In The Folder?")
+    time.sleep(3)
+
